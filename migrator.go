dd0b7de5e95ae3f70eb74152e85d5d3b4643aebe
Jinzhu <zhangjinzhu@bytedance.com>
2021-01-26 14:13:31 +0800

    Upgrade gorm to v1.20.12 & Refactor HasConstraint

--- migrator.go
+++ migrator.go
@@ -163,9 +163,16 @@ func (m Migrator) RenameIndex(value interface{}, oldName, newName string) error 
 func (m Migrator) HasConstraint(value interface{}, name string) bool {
 	var count int64
 	m.RunWithValue(value, func(stmt *gorm.Statement) error {
+		constraint, chk, table := m.GuessConstraintAndTable(stmt, name)
+		if constraint != nil {
+			name = constraint.Name
+		} else if chk != nil {
+			name = chk.Name
+		}
+
 		return m.DB.Raw(
 			`SELECT count(*) FROM sys.foreign_keys as F inner join sys.tables as T on F.parent_object_id=T.object_id inner join information_schema.tables as I on I.TABLE_NAME = T.name WHERE F.name = ?  AND T.Name = ? AND I.TABLE_CATALOG = ?;`,
-			name, stmt.Table, m.CurrentDatabase(),
+			name, table, m.CurrentDatabase(),
 		).Row().Scan(&count)
 	})
 	return count > 0

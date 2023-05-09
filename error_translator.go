cae90ebf04071848095d2c1e0f5b9f0034b862e4
Saeid Saeidee <s.saeidee@sensysgatso.com>
2023-05-09 13:02:03 +0200

    feat: error translation support

---
+++ error_translator.go
@@ -0,0 +1,27 @@
+package sqlserver
+
+import (
+	"github.com/microsoft/go-mssqldb"
+
+	"gorm.io/gorm"
+)
+
+var errCodes = map[string]int32{
+	"uniqueConstraint": 2627,
+}
+
+type ErrMessage struct {
+	Number  int32  `json:"Number"`
+	Message string `json:"Message"`
+}
+
+// Translate it will translate the error to native gorm errors.
+func (dialector Dialector) Translate(err error) error {
+	if mssqlErr, ok := err.(*mssql.Error); ok {
+		if mssqlErr.Number == errCodes["uniqueConstraint"] {
+			return gorm.ErrDuplicatedKey
+		}
+	}
+
+	return err
+}

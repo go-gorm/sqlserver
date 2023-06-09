6c6d08e1d97217f71c8cbfca6326e6dc4269d6fc
Saeid <sk.saeidee@yahoo.com>
2023-06-09 04:22:43 +0200

    fix: error translation casting (#103)
    Co-authored-by: Saeid Saeidee <s.saeidee@sensysgatso.com>

--- error_translator.go
+++ error_translator.go
@@ -6,6 +6,7 @@ import (
 	"gorm.io/gorm"
 )
 
+// The error codes to map mssql errors to gorm errors, here is a reference about error codes for mssql https://learn.microsoft.com/en-us/sql/relational-databases/errors-events/database-engine-events-and-errors?view=sql-server-ver16
 var errCodes = map[string]int32{
 	"uniqueConstraint": 2627,
 }
@@ -17,7 +18,7 @@ type ErrMessage struct {
 
 // Translate it will translate the error to native gorm errors.
 func (dialector Dialector) Translate(err error) error {
-	if mssqlErr, ok := err.(*mssql.Error); ok {
+	if mssqlErr, ok := err.(mssql.Error); ok {
 		if mssqlErr.Number == errCodes["uniqueConstraint"] {
 			return gorm.ErrDuplicatedKey
 		}

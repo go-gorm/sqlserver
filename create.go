37e75a1f4e71e94f37ff8a69a1a8dcc3cc4e6f9b
Joseph Chang <joseph@newtype.com.tw>
2023-11-27 11:58:36 +0800

    to resolve https://github.com/go-gorm/gorm/issues/6708

--- create.go
+++ create.go
@@ -135,7 +135,7 @@ func Create(db *gorm.DB) {
 
 func MergeCreate(db *gorm.DB, onConflict clause.OnConflict, values clause.Values) {
 	db.Statement.WriteString("MERGE INTO ")
-	db.Statement.WriteQuoted(db.Statement.Table)
+	db.Statement.WriteString(db.Statement.TableExpr.SQL)
 	db.Statement.WriteString(" USING (VALUES")
 	for idx, value := range values.Values {
 		if idx > 0 {

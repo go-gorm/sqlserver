4f2c3fea9f319b105a818d6f6451254a9655cd93
iTanken <23544702+iTanken@users.noreply.github.com>
2025-05-25 08:44:20 +0800

    fix: GetIndexes method returns zero values (#149)
    due to the mismatch between the `Index` struct field order and the `indexSQL` field order

--- migrator_test.go
+++ migrator_test.go
@@ -1,6 +1,7 @@
 package sqlserver_test
 
 import (
+	"encoding/json"
 	"os"
 	"reflect"
 	"testing"
@@ -247,5 +248,58 @@ func TestMigrator_MigrateColumnComment(t *testing.T) {
 			t.Fatalf("expected comments %#v, got %#v", wantComments, gotComments)
 		}
 		t.Logf("got comments: %#v", gotComments)
+	}
+}
+
+func TestMigrator_GetIndexes(t *testing.T) {
+	db, err := gorm.Open(sqlserver.Open(sqlserverDSN))
+	if err != nil {
+		t.Fatal(err)
+	}
+	dm := db.Debug().Migrator()
+
+	type testTableIndex struct {
+		Test uint64 `gorm:"index"`
+	}
+	type testTableUnique struct {
+		ID string `gorm:"index:unique_id,class:UNIQUE,where:id IS NOT NULL"`
+	}
+	type testTablePrimaryKey struct {
+		ID string `gorm:"primaryKey"`
+	}
+
+	type args struct {
+		value interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{name: "index", args: args{value: new(testTableIndex)}},
+		{name: "unique", args: args{value: new(testTableUnique)}},
+		{name: "primaryKey", args: args{value: new(testTablePrimaryKey)}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err = dm.AutoMigrate(tt.args.value); err != nil {
+				t.Error(err)
+			}
+			got, gotErr := dm.GetIndexes(tt.args.value)
+			if (gotErr != nil) != tt.wantErr {
+				t.Errorf("GetIndexes() error = %v, wantErr %v", gotErr, tt.wantErr)
+				return
+			}
+			for _, index := range got {
+				_, validUnique := index.Unique()
+				_, validPK := index.PrimaryKey()
+				indexBytes, _ := json.Marshal(index)
+				if index.Name() == "" && !validUnique && !validPK {
+					t.Errorf("GetIndexes() got = %s empty", indexBytes)
+				} else {
+					t.Logf("GetIndexes() got = %s", indexBytes)
+				}
+			}
+		})
 	}
 }
